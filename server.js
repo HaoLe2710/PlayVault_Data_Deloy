d92b9330c559032e6bdacc951ac4f654f179adea
HaoLe2710 <lengochao123.27102004@gmail.com>
2025-05-08 23:00:35 +0700

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // server.js
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('games_data.json');
-const middlewares = jsonServer.defaults();
+import { create, router as _router, defaults } from 'json-server';
+const server = create();
+const router = _router('games_data.json');
+const middlewares = defaults();
 
 server.use(middlewares);
 server.use(router);
